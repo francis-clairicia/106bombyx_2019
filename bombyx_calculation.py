64411e756fef40e2b0d575ce2289e3a3f84a7235
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-02-21 10:17:03 +0100

    Added negative generation handling

--- bombyx_calculation.py
+++ bombyx_calculation.py
@@ -1,18 +1,30 @@
 # -*- coding: Utf-8 -*
 
+def print_float(x: float, n: int):
+    str_x = str(x)
+    dot = str_x.find(".")
+    if n == 0:
+        print(str_x[:dot])
+    else:
+        print(str_x[:dot + 1 + n])
+
+def logistic_equation(x: float, k: float):
+    x = k * x * (1000 - x) / 1000
+    return 0 if x < 0 else x
+
 def calculation_with_given_k(n: int, k: float):
     x = n
     for i in range(100):
         print(f"{i + 1} {x:.2f}")
-        x = k * x * (1000 - x) / 1000
+        x = logistic_equation(x, k)
 
 def calculation_with_initial_generation(n: int, i0: int, i1: int):
     k = 1
     while k <= 4:
-        x = n
+        x = float(n)
         for _ in range(int(i0)):
-            x = k * x * (1000 - x) / 1000
+            x = logistic_equation(x, k)
         for _ in range(int(i0), int(i1) + 1):
-            print(f"{k:.2f} {x:.3f}")
-            x = k * x * (1000 - x) / 1000
+            print(f"{k:.2f} {round(x, 2):.2f}")
+            x = logistic_equation(x, k)
         k = round(k + 0.01, 2)
