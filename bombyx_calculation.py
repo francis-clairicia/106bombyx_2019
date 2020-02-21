3ce98b2ddb48717aea877bb8061efe521fd4ab7c
Francis Clairicia-Rose-Claire-Josephine <fclairicia-rcj@frankysnow.fr>
2020-02-21 10:28:37 +0100

    Use of 'decimal' module

--- bombyx_calculation.py
+++ bombyx_calculation.py
@@ -1,30 +1,24 @@
 # -*- coding: Utf-8 -*
 
-def print_float(x: float, n: int):
-    str_x = str(x)
-    dot = str_x.find(".")
-    if n == 0:
-        print(str_x[:dot])
-    else:
-        print(str_x[:dot + 1 + n])
+from decimal import Decimal
 
-def logistic_equation(x: float, k: float):
-    x = k * x * (1000 - x) / 1000
-    return 0 if x < 0 else x
+def logistic_equation(x: Decimal, k: Decimal):
+    x = k * x * (Decimal('1000') - x) / Decimal('1000')
+    return Decimal('0') if x < Decimal('0') else x
 
-def calculation_with_given_k(n: int, k: float):
+def calculation_with_given_k(n: Decimal, k: Decimal):
     x = n
     for i in range(100):
         print(f"{i + 1} {x:.2f}")
         x = logistic_equation(x, k)
 
-def calculation_with_initial_generation(n: int, i0: int, i1: int):
-    k = 1
+def calculation_with_initial_generation(n: Decimal, i0: Decimal, i1: Decimal):
+    k = Decimal('1')
     while k <= 4:
-        x = float(n)
+        x = n
         for _ in range(int(i0)):
             x = logistic_equation(x, k)
         for _ in range(int(i0), int(i1) + 1):
-            print(f"{k:.2f} {round(x, 2):.2f}")
+            print(f"{k:.2f} {x:.2f}")
             x = logistic_equation(x, k)
-        k = round(k + 0.01, 2)
+        k += Decimal('0.01')
